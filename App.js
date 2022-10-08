24a9ca477e072a582a673cdc7fd214f2f726d00e
Saurav Silawat <silawatsaurav@gmail.com>
2022-10-08 21:12:21 +0530

    CSS styling

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import ColorPickerComponent from './ColorPickerComponent'
+
+function App() {
+  return (  
+    <div className="App">  
+      <ColorPickerComponent />  
+    </div>  
+  );
+}
+
+export default App;
