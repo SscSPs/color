21b4993bbb70984b4127384d0b86d5c3e50af192
Vinit Hemadri <42197888+Vinitvh@users.noreply.github.com>
2022-10-09 11:45:05 +0530

    feat: Add Header closes #13 (#19)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import ColorPickerComponent from './ColorPickerComponent'
+import Navbar from './Navbar';
 
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <ColorPickerComponent />
     </div>
   )
