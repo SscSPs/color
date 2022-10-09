4d27c16c71c7c59292549197ad8bac71c9338b50
Michalis Pervolarakis <pervolarakes@gmail.com>
2022-10-09 18:52:13 +0300

    added noreferrer to anchor link

--- src/VisitGithub.js
+++ src/VisitGithub.js
@@ -1,7 +1,7 @@
 const VisitGithub = () => {
     return(
         <div className="visitGithub">
-            <a className="visitGithubBtn" href="https://github.com/SscSPs/color" target="_blank">
+            <a className="visitGithubBtn" href="https://github.com/SscSPs/color" rel="noreferrer" target="_blank">
                 <img src={process.env.PUBLIC_URL + '/GitHub-Mark-Light-32px.png'} alt="img"/>
             </a>
         </div>
