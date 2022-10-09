7ae691c360ba55dd81367621f46d3f7fa00bc33f
Newton Fernandis <newtonb.is18@sahyadri.edu.in>
2022-10-09 12:52:02 +0530

    feature #22 added complementary color section

--- src/ColorGrid.js
+++ src/ColorGrid.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import reactCSS from 'reactcss';
-import './App.css';
-import { hsl } from 'color-convert';
+import React from "react";
+import reactCSS from "reactcss";
+import "./App.css";
+import { hsl } from "color-convert";
 
 const ColorGrid = ({ hueValue, saturationCount, luminosityCount }) => {
   const { outerWidth: width, outerHeight: height } = window;
@@ -14,56 +14,60 @@ const ColorGrid = ({ hueValue, saturationCount, luminosityCount }) => {
 
   return (
     <div
-      className='colorgrid'
-      style={{ border: '1px', borderBlock: '10px', borderColor: 'red' }}
+      className="colorgrid"
+      style={{ border: "1px", borderBlock: "10px", borderColor: "red" }}
     >
       {
-        // create saturationCount divs
-        Array(saturationCount - 1)
-          .fill(0)
-          .map((_, i) => {
-            return (
-              <div key={i} style={{ display: 'flex', flexDirection: 'row' }}>
-                {
-                  // create luminosityCount divs
-                  Array(luminosityCount - 1)
-                    .fill(0)
-                    .map((_, j) => {
-                      var hueLocal = hueValue;
-                      var saturationLocal = j * saturationFraction;
-                      var luminosityLocal = j * luminosityFraction;
-                      const hexColour = hsl.hex(hueLocal,saturationLocal,luminosityLocal);
-                      const styles = reactCSS({
-                        default: {
-                          color: {
-                            width: `${oneColorWidth}px`,
-                            height: `${oneColorHeight}px`,
-                            borderRadius: '5px',
-                            background: `hsl(${hueLocal}, ${saturationLocal}%, ${luminosityLocal}%)`,
-                            color: luminosityLocal > 50 ? "#000" : "#fff",
-                          },
-                          swatch: {
-                            padding: '1px',
-                            background: '#fff',
-                            borderRadius: '5px',
-                            boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-                            display: 'inline-block',
-                            cursor: 'pointer',
-                          },
-                        },
-                      });
-                      return (
-                        <div key={j} style={styles.swatch}>
-                          <div style={styles.color} className="colour-cell" onClick={() => {navigator.clipboard.writeText(hexColour)}}>
-                            <p>{hexColour}</p>
-                          </div>
-                        </div>
-                      );
-                    })
-                }
-              </div>
-            );
-          })
+        // create saturationCount div
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          {
+            // create luminosityCount divs
+            Array(luminosityCount - 1)
+              .fill(0)
+              .map((_, j) => {
+                var hueLocal = hueValue;
+                var saturationLocal = j * saturationFraction;
+                var luminosityLocal = j * luminosityFraction;
+                const hexColour = hsl.hex(
+                  hueLocal,
+                  saturationLocal,
+                  luminosityLocal
+                );
+                const styles = reactCSS({
+                  default: {
+                    color: {
+                      width: `${oneColorWidth}px`,
+                      height: `${oneColorHeight}px`,
+                      borderRadius: "5px",
+                      background: `hsl(${hueLocal}, ${saturationLocal}%, ${luminosityLocal}%)`,
+                      color: luminosityLocal > 50 ? "#000" : "#fff",
+                    },
+                    swatch: {
+                      padding: "1px",
+                      background: "#fff",
+                      borderRadius: "5px",
+                      boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
+                      display: "inline-block",
+                      cursor: "pointer",
+                    },
+                  },
+                });
+                return (
+                  <div key={j} style={styles.swatch}>
+                    <div
+                      style={styles.color}
+                      className="colour-cell"
+                      onClick={() => {
+                        navigator.clipboard.writeText(hexColour);
+                      }}
+                    >
+                      <p>{hexColour}</p>
+                    </div>
+                  </div>
+                );
+              })
+          }
+        </div>
       }
     </div>
   );
--- src/ColorPickerComponent.js
+++ src/ColorPickerComponent.js
@@ -1,34 +1,48 @@
-import React, { useState } from 'react';
-import { SketchPicker } from 'react-color';
-import { rgb } from 'color-convert';
-import ColorGrid from './ColorGrid';
-
-
+import React, { useState } from "react";
+import { SketchPicker } from "react-color";
+import { rgb } from "color-convert";
+import ColorGrid from "./ColorGrid";
+import "./App.css";
 const ColorPickerComponent = () => {
-//   const [displayColorPicker, setDisplayColorPicker] = useState(false);
+  //   const [displayColorPicker, setDisplayColorPicker] = useState(false);
   const [color, setColor] = useState({ r: 241, g: 112, b: 19 });
 
-//   const handleClick = () => setDisplayColorPicker((prev) => !prev);
+  //   const handleClick = () => setDisplayColorPicker((prev) => !prev);
 
-//   const handleClose = () => setDisplayColorPicker(false);
+  //   const handleClose = () => setDisplayColorPicker(false);
 
   const handleChange = (color) => setColor(color.rgb);
 
   const colorAsHSL = rgb.hsl(color.r, color.g, color.b);
 
   return (
-    <div className='main'>
-      <div>
-        <ColorGrid
-          hueValue={colorAsHSL[0]}
-          saturationCount='15'
-          luminosityCount='15'
-        />
+    <>
+      <div className="main">
+        <div>
+          <div className="color-section">
+            <h3 style={{ color: "#ffffff", fontWeight: "bold" }}>PRIMARY</h3>
+            <ColorGrid
+              hueValue={colorAsHSL[0]}
+              saturationCount="15"
+              luminosityCount="15"
+            />
+          </div>
+          <div>
+            <h3 style={{ color: "#ffffff", fontWeight: "bold" }}>
+              COMPLEMENTARY
+            </h3>
+            <ColorGrid
+              hueValue={colorAsHSL[0] + 180}
+              saturationCount="15"
+              luminosityCount="15"
+            />
+          </div>
+        </div>
+        <div className="colorpick">
+          <SketchPicker color={color} onChange={handleChange} />
+        </div>
       </div>
-      <div className='colorpick'>
-        <SketchPicker color={color} onChange={handleChange} />
-      </div>
-    </div>
+    </>
   );
 };
 
