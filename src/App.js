1a5adee7981de76b8678e5cde7f0011856eeefd4
Piyush Kumar <piyushprasad816@gmail.com>
2022-10-09 21:38:00 -0700

    added toast message

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import ColorPickerComponent from './ColorPickerComponent'
-import Navbar from './Navbar';
-import VisitGithub from './VisitGithub';
-
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import ColorPickerComponent from "./ColorPickerComponent";
+import Navbar from "./Navbar";
+import VisitGithub from "./VisitGithub";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 const App = () => {
   return (
     <div className="App">
       <VisitGithub />
       <Navbar />
+      <ToastContainer />
       <ColorPickerComponent />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/ColorGrid.js
+++ src/ColorGrid.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import reactCSS from 'reactcss';
-import './App.css';
-import { hsl } from 'color-convert';
+import React from "react";
+import reactCSS from "reactcss";
+import "./App.css";
+import { hsl } from "color-convert";
+import { toast } from "react-toastify";
 
 const ColorGrid = ({ hueValue, saturationCount, luminosityCount }) => {
   const { outerWidth: width, outerHeight: height } = window;
@@ -14,8 +15,8 @@ const ColorGrid = ({ hueValue, saturationCount, luminosityCount }) => {
 
   return (
     <div
-      className='colorgrid'
-      style={{ border: '1px', borderBlock: '10px', borderColor: 'red' }}
+      className="colorgrid"
+      style={{ border: "1px", borderBlock: "10px", borderColor: "red" }}
     >
       {
         // create saturationCount divs
@@ -23,7 +24,10 @@ const ColorGrid = ({ hueValue, saturationCount, luminosityCount }) => {
           .fill(0)
           .map((_, saturationStep) => {
             return (
-              <div key={saturationStep} style={{ display: 'flex', flexDirection: 'row' }}>
+              <div
+                key={saturationStep}
+                style={{ display: "flex", flexDirection: "row" }}
+              >
                 {
                   // create luminosityCount divs
                   Array(luminosityCount - 1)
@@ -32,30 +36,47 @@ const ColorGrid = ({ hueValue, saturationCount, luminosityCount }) => {
                       var hueLocal = hueValue;
                       var saturationLocal = saturationStep * saturationFraction;
                       var luminosityLocal = luminosityStep * luminosityFraction;
-                      const hexColour = hsl.hex(hueLocal, saturationLocal, luminosityLocal);
+                      const hexColour = hsl.hex(
+                        hueLocal,
+                        saturationLocal,
+                        luminosityLocal
+                      );
                       const styles = reactCSS({
                         default: {
                           color: {
                             width: `${oneColorWidth}px`,
                             height: `${oneColorHeight}px`,
-                            borderRadius: '5px',
+                            borderRadius: "5px",
                             background: `hsl(${hueLocal}, ${saturationLocal}%, ${luminosityLocal}%)`,
                             color: luminosityLocal > 50 ? "#000" : "#fff",
                           },
                           swatch: {
-                            padding: '1px',
-                            background: '#fff',
-                            borderRadius: '5px',
-                            boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-                            display: 'inline-block',
-                            cursor: 'pointer',
+                            padding: "1px",
+                            background: "#fff",
+                            borderRadius: "5px",
+                            boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
+                            display: "inline-block",
+                            cursor: "pointer",
                           },
                         },
                       });
                       return (
                         <div key={luminosityStep} style={styles.swatch}>
-                          <div style={styles.color} className="color-cell" onClick={() => { navigator.clipboard.writeText(`#${hexColour}`) }}>
-                            <p>{hexColour}</p>
+                          <div
+                            style={styles.color}
+                            className="color-cell"
+                            onClick={() => {
+                              navigator.clipboard
+                                .writeText(`#${hexColour}`)
+                                .then(() => {
+                                  toast.success("Color copied.");
+                                })
+                                .catch(() => {
+                                  toast.error("Failed to copy");
+                                });
+                            }}
+                          >
+                            <p>#{hexColour}</p>
                           </div>
                         </div>
                       );
