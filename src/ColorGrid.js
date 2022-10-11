631281da2c359465446e99f2ee58776ada14577d
Rohit <95734284+Rohit-Saini7@users.noreply.github.com>
2022-10-11 22:43:23 +0530

    add styling for mobile version (#64)
    Co-authored-by: rohit.saini1 <rohit.saini@valyu.ai>

--- src/ColorGrid.js
+++ src/ColorGrid.js
@@ -10,9 +10,13 @@ const ColorGrid = ({ hueValue, saturationCount, luminosityCount }) => {
   const saturationFraction = 100 / (saturationCount - 2);
   const luminosityFraction = 100 / (luminosityCount - 2);
 
-  const oneColorWidth = width / (saturationCount - 2) / 2;
-  const oneColorHeight = height / (luminosityCount - 2) / 2;
+  let oneColorWidth = width / (saturationCount - 2) / 2;
+  let oneColorHeight = height / (luminosityCount - 2) / 2;
 
+  if (oneColorWidth < 40) {
+    oneColorHeight = 40;
+    oneColorWidth = 40;
+  }
   return (
     <div
       className='colorgrid'
@@ -46,22 +50,16 @@ const ColorGrid = ({ hueValue, saturationCount, luminosityCount }) => {
                           color: {
                             width: `${oneColorWidth}px`,
                             height: `${oneColorHeight}px`,
-                            borderRadius: '5px',
                             background: `hsl(${hueLocal}, ${saturationLocal}%, ${luminosityLocal}%)`,
                             color: luminosityLocal > 50 ? '#000' : '#fff',
-                          },
-                          swatch: {
-                            padding: '1px',
-                            background: '#fff',
-                            borderRadius: '5px',
-                            boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-                            display: 'inline-block',
-                            cursor: 'pointer',
                           },
                         },
                       });
                       return (
-                        <div key={luminosityStep} style={styles.swatch}>
+                        <div
+                          key={luminosityStep}
+                          className='color-cell-wrapper'
+                        >
                           <div
                             style={styles.color}
                             className='color-cell'
@@ -75,9 +73,8 @@ const ColorGrid = ({ hueValue, saturationCount, luminosityCount }) => {
                                   toast.error('Failed to copy');
                                 });
                             }}
-                          >
-                            <p>#{hexColour}</p>
-                          </div>
+                            title={`#${hexColour}`}
+                          />
                         </div>
                       );
                     })
