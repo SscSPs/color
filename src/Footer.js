f80be446b6d9a8e9cee30f046366aa9b93b7375a
Abhishek vaishnav <64257772+abhishek0777@users.noreply.github.com>
2022-10-14 16:46:02 +0530

    made UI better and added logo (#65)
    * made UI better and added logo
    
    * made UI better and added logo and fixed build error
    
    * made UI better and added logo and fixed build error
    
    * made changes as you said

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,16 @@
+import React from "react";
+import { SocialIcon } from 'react-social-icons';
 const year = new Date().getFullYear();
+
+
 const Footer = () => (
   <footer className='footer'>
-    <span>© {year} Copyright — </span>
-    <a title='My Twitter profile' href='https://twitter.com/heyitssahilsoni'>
+    <span className='footer-text'>© {year} Copyright — </span>
+    <a title='My Twitter profile' href='https://twitter.com/heyitssahilsoni' target="_blank" rel="noopener noreferrer">
       heyitssahilsoni
+      <SocialIcon url="https://twitter.com/heyitssahilsoni" style={{ height: 25, width: 25 }}/>
     </a>
+    
   </footer>
 );
 
