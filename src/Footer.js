b96a6c3148fc0c287b66549b9b19323b825b337a
Abhishek Vaishnav <abhishek777vaishnav@gmail.com>
2022-10-14 13:27:49 +0530

    made UI better and added logo and fixed build error

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ const year = new Date().getFullYear();
 const Footer = () => (
   <footer className='footer'>
     <span className='footer-text'>© {year} Copyright — </span>
-    <a title='My Twitter profile' href='https://twitter.com/heyitssahilsoni' target="_blank">
+    <a title='My Twitter profile' href='https://twitter.com/heyitssahilsoni' target="_blank" rel="noopener noreferrer">
       heyitssahilsoni
       <SocialIcon url="https://twitter.com/heyitssahilsoni" style={{ height: 25, width: 25 }}/>
     </a>
